989ac02109ba8281e702f9e9080c04b1e7d983b6
halazila <baohuaxiong77@gmail.com>
2018-09-18 00:34:33 +0800

    add distrubuteSpider

---
+++ distributeSpider/HtmlDownloader.py
@@ -0,0 +1,14 @@
+# encoding:utf-8
+import requests
+class HtmlDownloader(object):
+	"""Html 下载器"""
+	def download(self, url):
+		if url is None:
+			return None
+		user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36'
+		headers = {'User-Agent':user_agent}
+		r = requests.get(url, headers=headers)
+		if r.status_code==200:
+			r.encoding='utf-8'
+			return r.text
+		return None
